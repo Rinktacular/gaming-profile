be509e7245009eaae1e767cb34304fda2d5f23cd
Ryan Humphries <rinktacular@gmail.com>
2023-12-15 20:59:39 -0500

    Added selects to /profile, not actually filtering at this time.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,12 +10,12 @@ export default function Page() {
 
   const params = useSearchParams();
   let code = params.get('code') || '';
+  let realms;
 
   const fetchData = useCallback(async () => {
     // If a User have yet to authorize the app to battle.net, we will not have a code.
     if (!code) return;
     await getAuthorizationCodeForWowProfileScope(code).then((authCode) => {
-      console.log(authCode);
       getWowUserProfileSummary().then((response) => {
         // Check to ensure response is not `null` before setting the data.
         if (response?.wow_accounts.length) {
@@ -35,6 +35,26 @@ export default function Page() {
       <p>Welcome to profile page <a href="/">Back to dashboard</a></p>
       <button><a target="_blank" href={OUATH_URI}>I want to allow game profile to see my wow characters</a></button>
       <hr />
+      <div className="flex justify-evenly">
+        <div>
+          <label>Faction</label>
+          <select className="text-sky-500">
+            {data?.wow_accounts[0]?.characters.length && data.wow_accounts[0].characters.map((character, i) => <option className="text-amber-500" key={i}>{character.faction.name}</option>)}
+          </select>
+        </div>
+        <div>
+          <label>Realm</label>
+          <select className="text-sky-500">
+            {data?.wow_accounts[0]?.characters.length && data.wow_accounts[0].characters.map((character, i) => <option className="text-amber-500" key={i}>{character.realm.name}</option>)}
+          </select>
+        </div>
+        <div>
+          <label>Level</label>
+          <select className="text-sky-500">
+            {data?.wow_accounts[0]?.characters.length && data.wow_accounts[0].characters.map((character, i) => <option className="text-amber-500" key={i}>{character.level}</option>)}
+          </select>
+        </div>
+      </div>
       <div className="flex justify-evenly w-full flex-wrap">
         {
           data?.wow_accounts[0]?.characters.length && data.wow_accounts[0].characters.map((character, i) => <WowProfile key={i} character={character} />)
--- src/app/ui/wow-profile.tsx
+++ src/app/ui/wow-profile.tsx
@@ -6,7 +6,7 @@ export default function WowProfile({ character }: { character: Character }) {
   const factionBorderColor = character.faction.type === 'HORDE' ? 'border-rose-500' : 'border-sky-500';
 
   return (
-    <div className={`flex flex-row justify-evenly border-solid border-2 ${factionBorderColor} rounded hover:border-dotted hover:border-amber-300 w-2/5 h-12 my-8 content-center`}>
+    <div className={`flex flex-row justify-evenly border-solid border-2 ${factionBorderColor} rounded hover:border-dotted hover:border-amber-300 w-2/5 h-12 my-8 content-center hover:cursor-pointer`}>
       <p className="text-amber-500 mt-2">{character.name}</p>
       <p className="text-amber-800 mt-2">{character.level}</p>
       <p className={`${factionColor} mt-2`}>{character.faction.name}</p>
